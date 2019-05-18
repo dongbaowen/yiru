52abd0cfec4a3bcff1f43da83c5bc1b538d99f4a
dongbaowen <dongbaowen92@126.com>
2019-05-18 13:52:57 +0800

    .

--- yiru-user/yiru-user-provider/yiru-user-impl/src/main/java/com/yiru/service/UserServiceImpl.java
+++ yiru-user/yiru-user-provider/yiru-user-impl/src/main/java/com/yiru/service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.yiru.service;
 
 import com.yiru.bean.User;
 import com.yiru.dao.UserDao;
-import org.apache.ibatis.binding.MapperProxy;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -18,8 +17,6 @@ public class UserServiceImpl implements UserService{
     private UserDao userDao;
 
     public List<User> getUserList() {
-        System.out.println(userDao.getClass().getName());
-        System.out.println(userDao instanceof MapperProxy);
         return userDao.getUserList();
     }
 
--- yiru-user/yiru-user-provider/yiru-user-web/target/yiru-user-web-1.0-SNAPSHOT/index.jsp
+++
@@ -1,5 +0,0 @@
-<html>
-<body>
-<h2>易如</h2>
-</body>
-</html>
